47c538245b26ef03ed8e9e50179182254823eacf
cancc <1051024728@qq.com>
2018-08-22 00:29:54 +0800

    init

---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="m-header">
+		<h1 class="text">Chicken Music</h1>
+	</div>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+@import '~common/stylus/variable'
+@import '~common/stylus/mixin'
+
+.m-header
+    position: relative
+    height: 44px
+    text-align: center
+    color: $color-theme
+    font-size: 0
+    .text
+      display: inline-block
+      vertical-align: top
+      line-height: 44px
+      font-size: $font-size-large
+  
+</style>
